4917dc94806c296b0007a46bde84045de6133fe9
Pablo Copes <pcopescolombo@alumnos.exa.unicen.edu.ar>
2023-04-14 19:18:40 -0300

    chequear ejercicio 8

--- TP4_PELICULAS_Y_VOLUNTARIOS.sql
+++ TP4_PELICULAS_Y_VOLUNTARIOS.sql
@@ -23,3 +23,54 @@ comisión.(P) */
 SELECT id_empleado, apellido
 FROM unc_esq_peliculas.empleado
 WHERE porc_comision IS NULL;
+
+/* Muestre el apellido y el identificador de la tarea de todos los voluntarios que no tienen
+coordinador.(V) */
+
+SELECT apellido, id_tarea
+FROM unc_esq_voluntario.voluntario
+WHERE id_coordinador IS NULL;
+
+/* Muestre los datos de los distribuidores internacionales que no tienen registrado teléfono.
+(P) */
+
+SELECT *
+FROM unc_esq_peliculas.distribuidor
+WHERE tipo = 'I' || telefono IS NULL;
+
+/* Muestre los apellidos, nombres y mails de los empleados con cuentas de gmail y cuyo
+sueldo sea superior a $ 1000. (P) */
+
+SELECT apellido, nombre, e_mail
+FROM unc_esq_peliculas.empleado
+WHERE sueldo > 1000 AND e_mail LIKE '%gmail.com';
+
+/* Seleccione los diferentes identificadores de tareas que se utilizan en la tabla empleado. (P) */
+
+SELECT DISTINCT id_tarea -- esto esta bien?
+FROM unc_esq_peliculas.empleado
+
+/* Muestre el apellido, nombre y mail de todos los voluntarios cuyo teléfono comienza con
++51. Coloque el encabezado de las columnas de los títulos &#39;Apellido y Nombre&#39; y &#39;Dirección
+de mail&#39;. (V)
+
+Hacer un listado de los cumpleaños de todos los empleados donde se muestre el nombre y
+el apellido (concatenados y separados por una coma) y su fecha de cumpleaños (solo el
+día y el mes), ordenado de acuerdo al mes y día de cumpleaños en forma ascendente. (P)
+
+Recupere la cantidad mínima, máxima y promedio de horas aportadas por los voluntarios
+nacidos desde 1990. (V)
+
+Listar la cantidad de películas que hay por cada idioma. (P)
+
+Calcular la cantidad de empleados por departamento. (P)
+
+Mostrar los códigos de películas que han recibido entre 3 y 5 entregas. (veces entregadas,
+NO cantidad de películas entregadas).
+
+¿Cuántos cumpleaños de voluntarios hay cada mes?
+
+¿Cuáles son las 2 instituciones que más voluntarios tienen?
+
+¿Cuáles son los id de ciudades que tienen más de un departamento?
+*/
