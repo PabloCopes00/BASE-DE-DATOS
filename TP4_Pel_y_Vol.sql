4650d8474585b564e51fbe9b51bcd3d7d54a9863
Pablo Copes <pcopescolombo@alumnos.exa.unicen.edu.ar>
2023-04-15 14:45:53 -0300

    ejercicio 15 terminado

--- TP4_Pel_y_Vol.sql
+++ TP4_Pel_y_Vol.sql
@@ -1,4 +1,4 @@
-/*Seleccione el identificador y nombre de todas las instituciones que son Fundaciones.(V) */
+-- Seleccione el identificador y nombre de todas las instituciones que son Fundaciones.(V)
 SELECT id_institucion, nombre_institucion
 FROM unc_esq_voluntario.institucion
 WHERE nombre_institucion LIKE 'FUNDACION%';
@@ -31,8 +31,7 @@ SELECT apellido, id_tarea
 FROM unc_esq_voluntario.voluntario
 WHERE id_coordinador IS NULL;
 
-/* Muestre los datos de los distribuidores internacionales que no tienen registrado teléfono.
-(P) */
+-- Muestre los datos de los distribuidores internacionales que no tienen registrado teléfono. (P)
 
 SELECT *
 FROM unc_esq_peliculas.distribuidor
@@ -45,16 +44,14 @@ SELECT apellido, nombre, e_mail
 FROM unc_esq_peliculas.empleado
 WHERE sueldo > 1000 AND e_mail LIKE '%gmail.com';
 
-/* Seleccione los diferentes identificadores de tareas que se utilizan en la tabla empleado. (P) */
+-- Seleccione los diferentes identificadores de tareas que se utilizan en la tabla empleado. (P)
 
 SELECT DISTINCT id_tarea
 FROM unc_esq_peliculas.empleado;
 
 /* Muestre el apellido, nombre y mail de todos los voluntarios cuyo teléfono comienza con
 +51. Coloque el encabezado de las columnas de los títulos 'Apellido y Nombre' y 'Dirección
-de mail'. (V)
-
-   */
+de mail'. (V) */
 
 SELECT apellido || ',' || nombre AS "Apellido , nombre", e_mail AS "Direccion de mail"
 FROM unc_esq_voluntario.voluntario
@@ -64,21 +61,49 @@ WHERE telefono like '+51%';
 el apellido (concatenados y separados por una coma) y su fecha de cumpleaños (solo el
 día y el mes), ordenado de acuerdo al mes y día de cumpleaños en forma ascendente. (P) */
 
-
+SELECT nombre ||','|| apellido AS "Nombre,Apellido",
+       extract(DAY FROM fecha_nacimiento) ||' / '||extract(MONTH FROM fecha_nacimiento) AS "Dia/mes"
+FROM unc_esq_peliculas.empleado
+ORDER BY extract(MONTH FROM fecha_nacimiento), extract(DAY FROM fecha_nacimiento);
 
 /*Recupere la cantidad mínima, máxima y promedio de horas aportadas por los voluntarios
-nacidos desde 1990. (V)
+nacidos desde 1990. (V) */
 
-Listar la cantidad de películas que hay por cada idioma. (P)
+SELECT MIN(horas_aportadas) AS Minimo, MAX(horas_aportadas) AS Maximo, AVG(horas_aportadas) AS Promedio, count(horas_aportadas)
+FROM unc_esq_voluntario.voluntario
+WHERE fecha_nacimiento >= '1990-01-01';
 
-Calcular la cantidad de empleados por departamento. (P)
+-- Listar la cantidad de películas que hay por cada idioma. (P)
 
-Mostrar los códigos de películas que han recibido entre 3 y 5 entregas. (veces entregadas,
-NO cantidad de películas entregadas).
+SELECT idioma, count(idioma)
+FROM unc_esq_peliculas.pelicula
+GROUP BY idioma HAVING count(idioma)>0;
 
-¿Cuántos cumpleaños de voluntarios hay cada mes?
+-- innecesario poner el HAVING pero esta bueno por si necesitamos contar por ej idiomas con mas o menos peliculas.
 
-¿Cuáles son las 2 instituciones que más voluntarios tienen?
+-- Calcular la cantidad de empleados por departamento. (P)
 
-¿Cuáles son los id de ciudades que tienen más de un departamento?
-*/
+SELECT id_departamento, count(id_empleado)
+FROM unc_esq_peliculas.empleado
+GROUP BY id_departamento
+ORDER BY id_departamento; -- esto esta de mas, pero esta bueno verlos ordenados.
+
+/* Mostrar los códigos de películas que han recibido entre 3 y 5 entregas. (veces entregadas,
+NO cantidad de películas entregadas). (P) */
+
+SELECT codigo_pelicula
+FROM unc_esq_peliculas.renglon_entrega
+WHERE nro_entrega BETWEEN 3 and 5;
+
+-- ¿Cuántos cumpleaños de voluntarios hay cada mes? (V)
+
+SELECT extract(MONTH FROM fecha_nacimiento) AS Mes, count(fecha_nacimiento) AS Cantidad_Cumpleaños
+FROM unc_esq_voluntario.voluntario
+GROUP BY extract(MONTH FROM fecha_nacimiento)
+ORDER BY extract(MONTH FROM fecha_nacimiento);
+
+-- ¿Cuáles son las 2 instituciones que más voluntarios tienen? (V)
+
+
+
+-- ¿Cuáles son los id de ciudades que tienen más de un departamento? (P)
